bc29ca214cb955c19535e6b02038a78b93a6f4d1
howard_yu <howard60211@gmail.com>
2019-08-04 18:17:00 +0800

    a

--- student.py
+++ student.py
@@ -1,6 +1,10 @@
 import socket
 
-connect_to_teacher = socket.socket()
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 8088
+
+connect_to_teacher = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect_to_teacher.bind(HOST, PORT)
 teacher_host = socket.gethostbyname("Macbook_Liu")
 student_host = socket.gethostbyname(socket.gethostname())
 connect_to_teacher.connect((teacher_host,8088))
--- teacher_server.py
+++ teacher_server.py
@@ -3,21 +3,19 @@ import socket
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8088
 # 建立一個socket對象
-server = socket.socket()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 綁定host,port
 server.bind((HOST, PORT))
 # 開始監聽
-server.listen(5)
+server.listen(1)
 # 等待客戶端連接
-conn, addr = server.accept()
-# 接受數據
 while True:
+    conn, addr = server.accept()
+    # 接受數據
     data = conn.recv(1024)
     print("student %s in %s has connect..." %(str(conn), str(addr)))
     if not data:
         print('客戶端斷開了')
     print(data)
-    conn.send(data.upper())
-
-# 關閉server
-server.close()
+    # 關閉server
+    server.close()
